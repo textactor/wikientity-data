e85734cc0338a5c10155099f35656c50687398c0
Dumitru Cantea <canteadumitru@gmail.com>
2018-05-02 18:18:39 +0300

    createOrUpdate

--- src/dynamo/wikiEntityRepository.ts
+++ src/dynamo/wikiEntityRepository.ts
@@ -2,5 +2,7 @@ import { DynamoRepository } from "./dynamoRepository";
 import { WikiEntity, IWikiEntityRepository } from "@textactor/wikientity-domain";
 
 export class WikiEntityRepository extends DynamoRepository<string, WikiEntity> implements IWikiEntityRepository {
-
+    createOrUpdate(data: WikiEntity): Promise<WikiEntity> {
+        return this.model.createOrUpdate(data);
+    }
 }
